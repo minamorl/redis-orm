c6fa346a9a2153b32d19f3377273bc0cbbdbca9e
minamorl <minamorl@users.noreply.github.com>
2015-11-13 05:40:32 +0900

    fix setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="redisorm",
+    name="redis-ormapper",
     description="Redis object relation mapper",
     author="minamorl",
     author_email="minamorl@minamorl.com",
