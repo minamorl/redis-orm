c85aed340fc4c53aacb9042b7b0eac8aab3ff866
minamorl <minamorl@users.noreply.github.com>
2015-12-10 19:37:26 +0900

    add test

--- tests/test_core.py
+++ tests/test_core.py
@@ -96,3 +96,4 @@ def test_delete2(test_redis):
     p.save(obj2)
     p.delete(obj)
     assert len(list(p.load_all(SamplePersistentObject))) == 2
+    assert list(p.load_all(SamplePersistentObject))[0] == None
