f3e24ba49665b43eaf09908d5a42893ebff612fe
minamorl <minamorl@users.noreply.github.com>
2015-12-12 23:44:52 +0900

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description="Redis object relation mapper",
     author="minamorl",
     author_email="minamorl@minamorl.com",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     tests_require=['tox'],
     install_requires=[
